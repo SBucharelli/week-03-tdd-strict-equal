cb11f8bf8f82908e6b5abb1c86850c36e84b9ebc
Stephanie Bucharelli <stephaniebucharelli@gmail.com>
2020-06-21 21:33:50 -0400

    Edited the .test.js file to use assert instead of expect; defined strictEqual f'n; both tests pass

--- tests/strict-equal.test.js
+++ tests/strict-equal.test.js
@@ -1,30 +1,30 @@
 const describe = require('mocha').describe
 const it = require('mocha').it
 const strictEqual = require('../strict-equal.js')
-const expect = require('chai').expect
+const assert = require('assert')
 
 describe('Comparison', () => {
   describe('strictEqual', () => {
     it('create a strictEqual function where 5 and "5" return false', () => {
       // setup test data
-      const falseData = [5, '5']
+      var falseData = [5, '5']
 
       // call the function with the test data
-      const outcome = strictEqual(falseData)
+      var outcome1 = strictEqual(falseData)
 
-      // assets what should be true
-      expect(outcome).to.equal(false)
+      // asserts what should be true
+      assert.equal(outcome1, false)
     })
 
     it('create a strictEqual function where "a" and "a" return true', () => {
       // setup test data
-      const trueData = ['a', 'a']
+      var trueData = ['a', 'a']
 
       // call the function with the test data
-      const outcome = strictEqual(trueData)
+      var outcome2 = strictEqual(trueData)
 
-      // assets what should be true
-      expect(outcome).to.equal(true)
+      // asserts what should be true
+      assert.equal(outcome2, true)
     })
   })
 })
